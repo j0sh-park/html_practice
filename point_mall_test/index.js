e65fed775a12c8f2b054169276c32da2d4f429b5
josh <7510543@gmail.com>
2019-07-25 10:53:23 +0900

    index items

--- point_mall_test/index.js
+++ point_mall_test/index.js
@@ -1,15 +1,36 @@
-$.ajax({
-    url: 'http://localhost:8003/items/',
-    type: 'get',
-    dataType: 'json'
-}).done((json) => {
-    console.log(json);
-});
+// $.ajax({
+//     url: 'http://localhost:8003/items/',
+//     type: 'get',
+//     dataType: 'json'
+// }).done((items) => {
+//     console.log(items);
+//     displayItems(items);
+// });
 
-var xmlHttp = new XMLHttpRequest();
-xmlHttp.onreadystatechange = function() { 
-    if (xmlHttp.readyState == 4)
-        console.log(xmlHttp.responseText);
+// var xmlHttp = new XMLHttpRequest();
+// xmlHttp.onreadystatechange = function() { 
+//     if (xmlHttp.readyState == 4) {
+//         const items = JSON.parse(this.responseText);
+//         console.log(items);
+//         displayItems(items);
+//     }
+// }
+// xmlHttp.open("GET", 'http://localhost:8003/items/', true);
+// xmlHttp.send(null);
+
+function displayItems(items) {
+    const itemListContainer = $('#item-list-container');
+    itemListContainer.empty();
+    for (const item of items) {
+        const $itemContainer = $(`
+        <div class="item-container">
+            <img src="${item.image}" alt="">
+            <div class="item-description">
+                    <p>${item.title}</p>
+                    <p>가격: ${item.price}</p>
+            </div>
+        </div>
+        `);
+        $itemContainer.appendTo(itemListContainer);
+    }
 }
-xmlHttp.open("GET", 'http://localhost:8003/items/', true);
-xmlHttp.send(null);
